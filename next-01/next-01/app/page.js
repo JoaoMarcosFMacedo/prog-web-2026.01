8e987983dccc914afb206894c7e726e2f961e48f
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-19 23:39:42 +0000

    Ainda tentando fazer algo

---
+++ next-01/next-01/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import "./globals.css";
+
+export default function Home() {
+  return (
+    <>
+    <div>
+      <h1>  Teste nskdnlskndlkfnslkdfnslkdnflksndflnslkdfn  </h1>
+    </div>
+    <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Illaoi_0.jpg"></img>
+    </>
+  );
+}
