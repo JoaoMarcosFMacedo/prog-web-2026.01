cb81552d38c6d4e5a1637129d902d9ed575a1f0d
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-02-24 00:29:24 +0000

    sçjdfç

--- PastaReact-02/react-testes/app/page.js
+++ PastaReact-02/react-testes/app/page.js
@@ -1,11 +1,22 @@
-import { Profile } from "../components/profiles";
+import { Profile, Profile2, Pi } from "../components/profiles";
+// import { Profile2} from "../components/profiles";
 
 export default function Illaoi(){
-  return (
-    <h1>
-      <Profile />
-      <Profile />
-    </h1>
-    
+  return(
+    <>
+      <selection>
+        Nome { 50 }
+        <h1> A logo marca da Barbie </h1>
+        <li>
+          <h2>Barbie</h2>
+          <Profile2/>
+        </li>
+        <li>
+          <h2>Ben 10</h2>
+          <Profile/>
+          Exemplo da expressão '{ 5 * 2 }' 
+        </li>
+      </selection>
+    </>
   )
 }
--- PastaReact-02/react-testes/components/profiles.js
+++ PastaReact-02/react-testes/components/profiles.js
@@ -1,8 +1,21 @@
 export function Profile() {
   return (
     <img
-      src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Illaoi_0.jpg"
-      alt="Illaoi"
+      src="https://resizing.flixster.com/D4M_ydBw195fVI-Hdlfuk07zlsE=/620x336/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8621182_i_h10_aa.jpg"
+      className="Barbie"
     />
   )
+}
+export function Profile2(){
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Barbie_Logo.svg"
+      className="Barbie"
+    />
+  )
+export fuincion Contante
+const perosna = {{
+
+  name = 'João'
+}
 }
