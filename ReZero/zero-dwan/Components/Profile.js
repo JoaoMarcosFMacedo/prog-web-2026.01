a4b734420d68494662c3e0237670f57e3c99442b
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-04-19 04:20:21 +0000

    ..

---
+++ ReZero/zero-dwan/Components/Profile.js
@@ -0,0 +1,9 @@
+export function Profile(){
+    return(
+        <div ClassName="fotoReact">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+            alt="React"
+            />
+        </div>
+    )
+}
